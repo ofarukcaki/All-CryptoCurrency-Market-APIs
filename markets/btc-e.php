fc32555a60c35d2b279387654491670225d61bb2
Ömer Faruk ÇAKI <OFarukCaki@users.noreply.github.com>
2017-05-12 14:11:02 +0300

    Update btc-e.php

--- markets/btc-e.php
+++ markets/btc-e.php
@@ -1,14 +1,14 @@
 <?php
 
 class btce{
-    function btc_usd($x,$y){
+    function get($x,$y){
         $btceSource = "https://btc-e.com/api/3/ticker/$x";
         $btceJson = file_get_contents($btceSource);
         $btce = json_decode($btceJson);
         //var_dump($btce);
-        return $btce->btc_usd->$y;
+        return $btce->get->$y;
     }
 }
 
 //$btce = new btce;
-//echo $btce->btc_usd('btc_usd','last');
+//echo $btce->get('btc_usd','last');
